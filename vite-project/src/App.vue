04892dc907af3f4e3b21608ca85463a51196de1f
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2023-04-25 13:50:38 -0400

    e2rwtyljk;l

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,13 +1,28 @@
 
 <script setup>
     import { data } from './getAPI.vue'
+    import App from "./App.vue"
+    import { createApp } from 'vue'
     console.log(data)
+    const app = createApp()
+    app.component("Point",Point)
 
+</script>
+<script>
+    import Point from './components/point.vue';
+
+export default {
+  components: {
+    Point
+  }
+}
 </script>
 
 
 <template>
-    <v-btn class="thing">    <thing /></v-btn>
-    </template>
+    <Point />
+</template>
+<script>
+</script>
 <style scoped>
 </style>
