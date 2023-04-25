98dd1dade3185678ad527b999aa060fe4f0c749d
nesslei <botchedbunnies@gmail.com>
2023-04-25 13:50:36 -0400

    Update NavBar.vue

--- vite-project/src/components/NavBar.vue
+++ vite-project/src/components/NavBar.vue
@@ -1,17 +1,5 @@
-<template>
-  <v-app-bar>
-    <v-btn>
-      <v-icon class="homeButton"> </v-icon>
-    </v-btn>
-    <v-spacer></v-spacer>
-    <v-btn text @click="scoll('home')" class="homeSectoin"> Home </v-btn>
-    <v-btn text @click="scoll('Biography')" class="homeSectoin">
-      Biography
-    </v-btn>
-    <v-btn text @click="scoll('Statistics')" class="homeSectoin">
-      Statistics
-    </v-btn>
-  </v-app-bar>
-</template>
+<template></template>
+
+<script class=""></script>
 
 <style></style>
