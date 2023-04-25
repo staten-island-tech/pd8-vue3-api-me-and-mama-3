936525e4ef2e51e76836773b20d8d662a82d6a6c
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2023-04-25 13:35:24 -0400

    idek anymore

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,11 +1,13 @@
+
 <script setup>
     import { data } from './getAPI.vue'
     console.log(data)
+
 </script>
 
+
 <template>
-
-</template>
-
+    <v-btn class="thing">    <thing /></v-btn>
+    </template>
 <style scoped>
 </style>
