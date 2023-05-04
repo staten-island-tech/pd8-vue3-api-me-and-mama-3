49f6714d3545eb68d29647cc435cd5eb4127e11e
VanesssaLei <botchedbunnies@gmail.com>
2023-05-03 22:03:47 -0500

    Merge branch 'main' into stevev2

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -17,19 +17,23 @@ let changeyear = function (x) {
 };
 </script>
 <template>
-  <point
-    @click="changeyear(-1)"
-    :text="'Previous Year'"
-    v-if="renderComponent"
-  ></point>
-  <point
-    @click="changeyear(1)"
-    :text="'Next Year'"
-    v-if="renderComponent"
-  ></point>
-  <navBar> </navBar>
-  <h2 v-if="renderComponent">{{ year }}</h2>
-  <barGraph :year="year" v-if="renderComponent"></barGraph>
-  <pieChart :year="year" v-if="renderComponent"></pieChart>
-  <h3 v-if="year == '2018'">No data for this year</h3>
+  <div>
+    <point
+      @click="changeyear(-1)"
+      :text="'Previous Year'"
+      v-if="renderComponent"
+    ></point>
+    <point
+      @click="changeyear(1)"
+      :text="'Next Year'"
+      v-if="renderComponent"
+    ></point>
+    <navBar> </navBar>
+    <h2 v-if="renderComponent">{{ year }}</h2>
+    <barGraph :year="year" v-if="renderComponent"></barGraph>
+    <pieChart :year="year" v-if="renderComponent"></pieChart>
+    <h3 v-if="year == '2018'">No data for this year</h3>
+  </div>
 </template>
+
+
